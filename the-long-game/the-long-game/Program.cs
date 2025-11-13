0b9b97a47bc6130e81ab67e9f9a98c86ce5b43d7
mksigrest <mksigrest@gmail.com>
2025-11-13 08:38:19 -0500

    added init code, start of console logs, added button press, started while and if logic

---
+++ the-long-game/the-long-game/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace theLongGame
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int score = 0;
+            bool i = false;
+
+            Console.Write("Hello Player, what is your name?: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "! Are you ready to compete?");
+            Console.WriteLine("Your button pressing begins now!");
+
+            while (i = false)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    i = true;
+                    return;
+                }
+            }
+
+            
+            Console.WriteLine($"You pressed: {keyInfo.Key}");
+        }
+    }
+}
